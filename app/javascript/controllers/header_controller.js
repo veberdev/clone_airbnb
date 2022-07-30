0f34d8861a98833a0b2dc7c59bb01d45ecdd5111
veber <weslusisa@gmail.com>
2022-07-30 17:43:40 -0300

    set up modal

--- app/javascript/controllers/header_controller.js
+++ app/javascript/controllers/header_controller.js
@@ -1,11 +1,18 @@
 import { Controller } from '@hotwired/stimulus'
-import { enter, leave, toggle } from 'el-transition'
-
+import { toggle } from 'el-transition'
 
 export default class extends Controller {
-  static targets = ['openUserMenu'];
+  static targets = ['openUserMenu', 'userAuthLink'];
+
   connect() {
     this.openUserMenuTarget.addEventListener('click', this.toggleDropdownMenu)
+
+    this.userAuthLinkTargets.forEach((link) => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        document.getElementById('modal-trigger').click();
+      });
+    });
   }
 
   toggleDropdownMenu() {
