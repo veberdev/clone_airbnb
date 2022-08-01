832387099b91af8be46ab386e895a66fe41e3c10
veber <weslusisa@gmail.com>
2022-08-01 15:52:41 -0300

    calculate distance with geolocation user logged in

---
+++ db/migrate/20220801183006_create_profiles.rb
@@ -0,0 +1,17 @@
+class CreateProfiles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :profiles do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :address_1
+      t.string :address_2
+      t.string :city
+      t.string :state
+      t.string :country
+      t.float :latitude
+      t.float :longitude
+      t.string :zip_code
+
+      t.timestamps
+    end
+  end
+end
