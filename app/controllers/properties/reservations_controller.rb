224bb295568afe3ca18202cc1a999df9d5a29e9f
veber <weslusisa@gmail.com>
2022-08-09 17:45:01 -0300

    build the reservation confirmation page

--- app/controllers/properties/reservations_controller.rb
+++ app/controllers/properties/reservations_controller.rb
@@ -7,6 +7,12 @@ module Properties
     def new
       @property = Property.find(params[:property_id])
       @reservation = @property.reservations.new
+      @checkin_date = new_reservation_params[:checkin_date]
+      @checkout_date = new_reservation_params[:checkout_date]
+      @subtotal = new_reservation_params[:subtotal]
+      @cleaning_fee = new_reservation_params[:cleaning_fee]
+      @service_fee = new_reservation_params[:service_fee]
+      @total = new_reservation_params[:total]
     end
 
     private
