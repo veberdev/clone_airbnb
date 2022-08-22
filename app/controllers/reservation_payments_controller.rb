eab1cf3b558ee8bdfc1f888fa84ccff4e7692c15
veber <weslusisa@gmail.com>
2022-08-22 09:30:39 -0300

    add specs for making reservations

--- app/controllers/reservation_payments_controller.rb
+++ app/controllers/reservation_payments_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class ReservationPaymentsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     stripe_card = Stripe::Customer.create_source(
       stripe_customer.id,
